e6e9edd0a7e423ca98fa310ca68d17ffed684138
ajiponzu <cpjvmm@outlook.jp>
2021-12-23 19:55:46 +0900

    クエリが正しく実行されないバグを残したまま開発停止

--- src/requests/queries/q1_5.rs
+++ src/requests/queries/q1_5.rs
@@ -34,6 +34,8 @@ fn query_1() -> String {
   #[derive(Debug)]
   struct Answer {
     #[allow(dead_code)]
+    group_name: String,
+    #[allow(dead_code)]
     min_ranking: i32,
     #[allow(dead_code)]
     max_ranking: i32,
@@ -44,6 +46,7 @@ fn query_1() -> String {
       row: &R,
     ) -> diesel::deserialize::Result<Self> {
       Ok(Answer {
+        group_name: row.get("group_name")?,
         min_ranking: row.get("Min(ranking)")?,
         max_ranking: row.get("Max(ranking)")?,
       })
@@ -53,17 +56,68 @@ fn query_1() -> String {
   let connection = establish_connection();
   let answers: Vec<Answer> = diesel::sql_query(
     "
-    Select Min(ranking), Max(ranking)
+    Select group_name, Min(ranking), Max(ranking)
     from countries Group By group_name;
     ",
   )
   .load(&connection)
   .unwrap();
-  format!("{:?}", answers)
+
+  let mut ret = String::new();
+  for answer in answers {
+    ret += &format!(
+      "{}, {}, {}\n",
+      answer.group_name, answer.min_ranking, answer.max_ranking
+    );
+  }
+  ret
 }
 
 fn query_2() -> String {
-  String::from("not implemented2.")
+  #[derive(Debug)]
+  struct Answer {
+    #[allow(dead_code)]
+    avgw: i32,
+    #[allow(dead_code)]
+    avgh: i32,
+  }
+
+  impl QueryableByName<DB> for Answer {
+    fn build<R: diesel::row::NamedRow<diesel::mysql::Mysql>>(
+      row: &R,
+    ) -> diesel::deserialize::Result<Self> {
+      Ok(Answer {
+        avgw: row.get("weight")?,
+        avgh: row.get("height")?,
+      })
+    }
+  }
+
+  let connection = establish_connection();
+  // let answers: Vec<Answer> = diesel::sql_query(
+  //   "
+  //   Select countries.name As country_name, Avg(players.height) As avg_height
+  //   from countries
+  //   Inner Join players
+  //   On countries.id = players.country_id
+  //   Group By countries.id;
+  //   ",
+  // )
+  let answers: Vec<Answer> = diesel::sql_query(
+    "
+    Select weight, height
+    From players
+    Where position = 'GK';
+    ",
+  )
+  .load(&connection)
+  .unwrap();
+
+  let mut ret = String::new();
+  for answer in answers {
+    ret += &format!("{}, {}\n", answer.avgw, answer.avgh);
+  }
+  ret
 }
 
 fn query_3() -> String {
@@ -85,10 +139,15 @@ mod tests {
   #[test]
   fn test() {
     assert_eq!(query(&0), "bad query");
-    assert_eq!(query(&1), "not implemented1.");
-    assert_eq!(query(&2), "not implemented2.");
+    assert_ne!(query(&1), "not implemented1.");
+    assert_ne!(query(&2), "not implemented2.");
     assert_eq!(query(&3), "not implemented3.");
     assert_eq!(query(&4), "not implemented4.");
     assert_eq!(query(&5), "not implemented5.");
+  }
+
+  #[test]
+  fn print_test() {
+    println!("{}", query(&2));
   }
 }
