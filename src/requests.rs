e26aa5754ba0f882ed86f61c9ddd47aefd70ed8a
ajiponzu <cpjvmm@outlook.jp>
2021-12-23 16:39:08 +0900

    テンプレートエンジン, コンテントタイプ追加

--- src/requests.rs
+++ src/requests.rs
@@ -4,7 +4,9 @@ use actix_web::{get, web, HttpResponse, Responder};
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
-  HttpResponse::Ok().body("Hello mysql-practice-server!")
+  HttpResponse::Ok()
+    .content_type("text/plain")
+    .body("Hello mysql-practice-server!")
 }
 
 #[get("/q{question_id}")]
@@ -14,9 +16,11 @@ pub async fn get_answer(web::Path(question_id): web::Path<String>) -> impl Respo
   if id != 0 {
     msg = queries::queries(&id)
   }
-  HttpResponse::Ok().body(msg)
+  HttpResponse::Ok()
+    .content_type("application/json")
+    .body(msg)
 }
 
 pub async fn manual_hello() -> impl Responder {
-  HttpResponse::Ok().body("Hello!")
+  HttpResponse::Ok().content_type("text/plain").body("Hello!")
 }
